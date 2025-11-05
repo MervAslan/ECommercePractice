8549d3231718cec4980ba7b147af686915ff28bb
MervAslan <merveaslan044@gmail.com>
2025-11-05 15:04:34 +0200

    Added ForeignKeyProductCategoryRelation

--- ecommerce.Models/Product.cs
+++ ecommerce.Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,5 +37,9 @@ namespace ecommerce.Models
         [Display(Name = "price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
     }
 }
