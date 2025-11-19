f8e02796895dc12ffb2152b63698310e9d9dd1ae
MervAslan <merveaslan044@gmail.com>
2025-11-19 14:50:58 +0200

    Assigned roles on registration

--- ecommerceWeb/Program.cs
+++ ecommerceWeb/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); //projede applicationdbcontext ihtiyac� olursa bana s�yle ben sana haz�r instance veiririm diyor.
 
-builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 builder.Services.AddRazorPages();
