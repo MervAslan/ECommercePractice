01ac3c23d64fef3e7372567ef3b5ab61451a567f
MervAslan <merveaslan044@gmail.com>
2025-11-20 15:33:08 +0200

    Added register other fields and fixed ui bugs

--- ecommerceWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ecommerceWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -107,6 +107,14 @@ namespace ecommerceWeb.Areas.Identity.Pages.Account
             public string? Role {  get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+            public string? StreetAddress { get; set; }
+            [Required]
+            public string? Name { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
         }
 
 
@@ -144,6 +152,13 @@ namespace ecommerceWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.State = Input.State;
+                user.PostalCode = Input.PostalCode;
+                user.PhoneNumber = Input.PhoneNumber;
+
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
@@ -195,7 +210,7 @@ namespace ecommerceWeb.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
