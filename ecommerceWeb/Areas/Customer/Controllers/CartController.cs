b2aa5852a7aecf3467fd12f2ad07e8dc8037f9d8
MervAslan <merveaslan044@gmail.com>
2025-11-25 15:55:06 +0200

    Added Cart controller and view

---
+++ ecommerceWeb/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ecommerceWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")] // area belirtmeyi unutma yoksa sayfalar açılmaz routingi belirtmek gerekiyor
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
