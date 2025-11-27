3fdd209adf59fb16d90fd8a07fd3c480f1704397
MervAslan <merveaslan044@gmail.com>
2025-11-27 09:40:07 +0200

    Added OrderDetail and  OrderHeader repositories

---
+++ ecommerce.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,25 @@
+﻿using ecommerce.DataAccess.Data;
+using ecommerce.DataAccess.Repository.IRepository;
+using ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecommerce.DataAccess.Repository
+{
+    public class OrderDetailRepository :Repository<OrderDetail>, IOrderDetailRepository 
+    {
+        private  ApplicationDbContext _db;
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetails.Update(obj);
+        }
+    }
+}
