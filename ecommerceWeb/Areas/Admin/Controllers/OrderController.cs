8b539f9416540f382dfe57b6e438c234401a8361
MervAslan <merveaslan044@gmail.com>
2025-12-31 11:43:57 +0200

    only admin and employee can see all orders

--- ecommerceWeb/Areas/Admin/Controllers/OrderController.cs
+++ ecommerceWeb/Areas/Admin/Controllers/OrderController.cs
@@ -2,9 +2,9 @@
 using ecommerce.Models;
 using ecommerce.Models.ViewModels;
 using ecommerce.Utility;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using System.Security.Claims;
 
 namespace ecommerceWeb.Areas.Admin.Controllers
 {
@@ -12,6 +12,8 @@ namespace ecommerceWeb.Areas.Admin.Controllers
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
+        public OrderVM OrderVM { get; set; } //her controllera parametre olarak göndermemize gerk yok çünkü bind property attribute'unu kullanıyoruz
         public OrderController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;   
@@ -22,36 +24,70 @@ namespace ecommerceWeb.Areas.Admin.Controllers
         }
         public IActionResult Details(int orderId)
         {
-            OrderVM orderVM= new()
+            OrderVM = new()
             {
                 OrderHeader = _unitOfWork.OrderHeader.Get(u => u.OrderHeaderId == orderId, includeProperties: "ApplicationUser"),
                 OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
             };
-            return View(orderVM);
+            return View(OrderVM);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = $"{SD.Role_Admin},{SD.Role_Employee}")] // burdaki , veya anlamına geliyor attribute içleri string tabanlı olduğu için c# expression çalışmaz
+        public IActionResult UpdateOrderDetail()
+        {
+            var orderHeaderFromDb = _unitOfWork.OrderHeader.Get(u => u.OrderHeaderId == OrderVM.OrderHeader.OrderHeaderId);
+            orderHeaderFromDb.Name = OrderVM.OrderHeader.Name;
+            orderHeaderFromDb.PhoneNumber = OrderVM.OrderHeader.PhoneNumber;
+            orderHeaderFromDb.StreetAddress = OrderVM.OrderHeader.StreetAddress;
+            orderHeaderFromDb.City = OrderVM.OrderHeader.City;
+            orderHeaderFromDb.State = OrderVM.OrderHeader.State;
+            orderHeaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
+            if (!string.IsNullOrEmpty(OrderVM.OrderHeader.Carrier)){
+                orderHeaderFromDb.Carrier = OrderVM.OrderHeader.Carrier;
+            }
+            if (!string.IsNullOrEmpty(OrderVM.OrderHeader.TrackingNumber))
+            {
+                orderHeaderFromDb.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+            }
+            _unitOfWork.OrderHeader.Update(orderHeaderFromDb);
+            _unitOfWork.Save();
+            TempData["success"] = "Order details updated successfully.";
+            return RedirectToAction(nameof(Details), new {orderId=orderHeaderFromDb.OrderHeaderId});
         }
         #region API CALLS
 
         [HttpGet]
         public IActionResult GetAll(string status)
         {
-            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
-            switch (status)
+            IEnumerable<OrderHeader> objOrderHeaders;
+            if(User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
             {
-                case "pending":
-                    objOrderHeaders = objOrderHeaders.Where(u=>u.PaymentStatus==SD.PaymentStatusDelayedPayment);
-                    break;
-                case "inprocess":
-                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
-                    break;
-                case "completed":
-                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
-                    break;
-                case "approved":
-                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
-                    break;
-                default:
-                   break;
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
             }
+            else
+            {
+                var claimsIdentity=(ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(u=>u.ApplicationUserId== userId, includeProperties:"ApplicationUser");
+            }
+                switch (status)
+                {
+                    case "pending":
+                        objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                        break;
+                    case "inprocess":
+                        objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                        break;
+                    case "completed":
+                        objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                        break;
+                    case "approved":
+                        objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                        break;
+                    default:
+                        break;
+                }
             return Json(new { data = objOrderHeaders});
         }
         
